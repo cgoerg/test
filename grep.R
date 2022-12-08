8ca333afa32b52c7137f81032ce97a15e29cdbe4
Carsten Görg <goerg@MtElbert.local>
2022-12-07 23:12:31 -0700

    jj

--- grep.R
+++ grep.R
@@ -9,10 +9,9 @@ n_drug_index <- grep("Nal",df$drug)
 either_drug_index <- grep("Bup|Nal",df$drug) 
 both_drugs_index <- grep("(?=.*Bup)(?=.*Nal)", df$drug, perl=T)
 
-bup <- rep(0,4)  # initialize bup with zeros
+bup <- rep(0,4)  # initialize bup with zeros bla bla
 nal <- rep(0,4)  # initialize nal with zeros
 bup[b_drug_index] <- 1  # use index to set bup rowx to 1
-nal[n_drug_index] <- 2  # use index to set nal rows to 2
 dx <- bup + nal         # add indices: rows with only bup remain 1 (1+0), rows with only nal remain 2(0+2), rows with both bup and nal will be 3 (1+2)
 
 dx
